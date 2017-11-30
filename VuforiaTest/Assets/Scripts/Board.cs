4f986568b0e1b3627f6cf99afaeca0a01c0526e0
Rasmus Fredrikson <rasmuf@kth.se>
2017-11-30 18:45:03 +0100

    commented the wasd controls

--- VuforiaTest/Assets/Scripts/Board.cs
+++ VuforiaTest/Assets/Scripts/Board.cs
@@ -24,26 +24,26 @@ public class Board : MonoBehaviour
 		//            Debug.LogError("Height" + i + " : " + height[i]);
 		//        }
 
-		if (Input.GetKeyDown("w"))
-		{
-            gameObject.transform.Rotate(new Vector3(-5, 0, 0));
-		}
+		//if (Input.GetKeyDown("w"))
+		//{
+  //          gameObject.transform.Rotate(new Vector3(-5, 0, 0));
+		//}
 
-		else if (Input.GetKeyDown("s"))
-		{
-			gameObject.transform.Rotate(new Vector3(5, 0, 0));
-		}
+		//else if (Input.GetKeyDown("s"))
+		//{
+		//	gameObject.transform.Rotate(new Vector3(5, 0, 0));
+		//}
 
-		else if (Input.GetKeyDown("a"))
-		{
-			gameObject.transform.Rotate(new Vector3(0, 0, -5));
-		}
+		//else if (Input.GetKeyDown("a"))
+		//{
+		//	gameObject.transform.Rotate(new Vector3(0, 0, -5));
+		//}
 
-		else if (Input.GetKeyDown("d"))
-		{
-			gameObject.transform.Rotate(new Vector3(0, 0, 5));
-		}
+		//else if (Input.GetKeyDown("d"))
+		//{
+		//	gameObject.transform.Rotate(new Vector3(0, 0, 5));
+		//}
 
-		//gameObject.transform.rotation = Quaternion.Euler(new Vector3(height[0]*45 - height[2]*45, 0, height[1]*45 - height[3]*45));
+		gameObject.transform.rotation = Quaternion.Euler(new Vector3(height[0]*45 - height[2]*45, 0, height[1]*45 - height[3]*45));
     }
 }
