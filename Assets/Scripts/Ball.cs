bd2305f346ace23dd67de0fadb41b78d8a1cc33f
Rasmus Fredrikson <rasmuf@kth.se>
2017-12-03 17:43:52 +0100

    added camera auto-focus

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,97 +1,111 @@
-﻿﻿using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using System.Text;
 using System.IO;
 using System;
 
-public class Ball : MonoBehaviour {
+public class Ball : MonoBehaviour
+{
 
-	float timeToFinishLine = 0f;
+    float timeToFinishLine = 0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	// Update is called once per frame
-	void Update () {
-		timeToFinishLine += Time.deltaTime;
-        gameObject.transform.Rotate(1,1,1); //Keeps the ball from getting stuck
-		if (gameObject.transform.position.y < -30f) {
-            ResetBall();
-		}
-
-		if (Input.GetKeyDown("h"))
-		{
-            ReadHighscore();
-        }
-		if (Input.GetKeyDown("r"))
-		{
-            ResetBall();
-		}
-	}
-
-	void OnTriggerEnter (Collider other){
-		if (other.tag == "Finish") {
-			Debug.Log ("You have reached the goal");
-			SaveHighscore ();
-            ResetBall();
-		}
-	}
-
-    private void ResetBall() {
-		gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-		gameObject.transform.position = new Vector3(-3.92f, 4.0f, -4.06f);
-		timeToFinishLine = 0;
     }
 
-    void SaveHighscore() {
-		string filePath = @"./Highscore.csv";  
-		string delimiter = ",";   
-		string[][] output;
-		string timeToFinishLineStr = timeToFinishLine.ToString();
-		string timeStamp = DateTime.Now.ToString();
-		string[] resultRow = new string[]{ timeToFinishLineStr, timeStamp};
+    // Update is called once per frame
+    void Update()
+    {
+        timeToFinishLine += Time.deltaTime;
+        gameObject.transform.Rotate(1, 1, 1); //Keeps the ball from getting stuck
+        if (gameObject.transform.position.y < -30f)
+        {
+            ResetBall();
+        }
 
-		if (!File.Exists (filePath)) {
-			Debug.Log ("No highscore file found, creating a new one");
-			output = new string[][] {
-				new string[]{ "timetofinishline", "timestamp" },  
-				resultRow
-			}; 
-		} else {
-			output = new string[][] {
-				resultRow
-			}; 
-		}
+        if (Input.GetKeyDown("h"))
+        {
+            ReadHighscore();
+        }
+        if (Input.GetKeyDown("r"))
+        {
+            ResetBall();
+        }
+    }
 
-		int length = output.GetLength(0);  
-		StringBuilder sb = new StringBuilder();  
-		for (int index = 0; index < length; index++)  
-			sb.AppendLine(string.Join(delimiter, output[index]));  
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Finish")
+        {
+            Debug.Log("You have reached the goal");
+            SaveHighscore();
+            ResetBall();
+        }
+    }
 
-		File.AppendAllText(filePath, sb.ToString());    
+    private void ResetBall()
+    {
+        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        gameObject.transform.position = new Vector3(-3.92f, 4.0f, -4.06f);
+        timeToFinishLine = 0;
+    }
 
-	}
+    void SaveHighscore()
+    {
+        string filePath = @"./Highscore.csv";
+        string delimiter = ",";
+        string[][] output;
+        string timeToFinishLineStr = timeToFinishLine.ToString();
+        string timeStamp = DateTime.Now.ToString();
+        string[] resultRow = new string[] { timeToFinishLineStr, timeStamp };
 
-	void ReadHighscore(){
-		List<string[]> highScores = new List<string[]>();
+        if (!File.Exists(filePath))
+        {
+            Debug.Log("No highscore file found, creating a new one");
+            output = new string[][] {
+                new string[]{ "timetofinishline", "timestamp" },
+                resultRow
+            };
+        }
+        else
+        {
+            output = new string[][] {
+                resultRow
+            };
+        }
 
-		using(var reader = new StreamReader(@"./Highscore.csv"))
-		{ 
-			while (!reader.EndOfStream)
-			{
-				var line = reader.ReadLine();
-				var values = line.Split(',');
-				highScores.Add(values);
-			}
-		}
-		foreach(var highScore in highScores) {
-			var highScorePrint = "";
-			foreach (var column in highScore) {
-				highScorePrint += " : " + column;
-			}
-			Debug.Log(highScorePrint);
-		}
-	}
+        int length = output.GetLength(0);
+        StringBuilder sb = new StringBuilder();
+        for (int index = 0; index < length; index++)
+            sb.AppendLine(string.Join(delimiter, output[index]));
+
+        File.AppendAllText(filePath, sb.ToString());
+
+    }
+
+    void ReadHighscore()
+    {
+        List<string[]> highScores = new List<string[]>();
+
+        using (var reader = new StreamReader(@"./Highscore.csv"))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                highScores.Add(values);
+            }
+        }
+        foreach (var highScore in highScores)
+        {
+            var highScorePrint = "";
+            foreach (var column in highScore)
+            {
+                highScorePrint += " : " + column;
+            }
+            Debug.Log(highScorePrint);
+        }
+    }
 }
